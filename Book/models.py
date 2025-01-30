c7e85e571899a2324bf4056f1b162247a0f68029
Pedram <amirabbasasgary9731@gmail.com>
2025-01-31 00:34:45 +0330

    feat: Basic model of Book writed. and basic view and url created.

--- Book/models.py
+++ Book/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    translator = models.CharField(max_length=100, blank=True, null=True)
+    publisher = models.CharField(max_length=100)
+    year = models.IntegerField()
+    pages = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Book'
--- Book/views.py
+++ Book/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from msilib.schema import ListView
+from django.shortcuts import render, HttpResponse
+from django.views import View
+from Book.models import Book
 
-# Create your views here.
+
+class BookListView(View):
+    model = Book
+    def get(self, request):
+        return HttpResponse('Book List')
+
+
