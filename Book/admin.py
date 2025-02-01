3896a4dc8ad2db3d75685f8759f63021f1b06dfc
Pedram <amirabbasasgary9731@gmail.com>
2025-02-01 13:06:55 +0330

    feat: Book Admin panel added.

--- Book/admin.py
+++ Book/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'publisher')
+
+
+    # list_filter = ('publisher', 'author')
+    # search_fields = ('title', 'author')
+    # prepopulated_fields = {'slug': ('title',)}
+    # date_hierarchy = 'publisher'
+    # ordering = ('publisher', 'title')
+    # empty_value_display = '-empty-'
+    # def save_model(self, request, obj, form, change):
+    #     obj.author = request.user
+    #     obj.publisher = request.user
+    #     obj.save()
+    #     super(BookAdmin, self).save_model(request, obj, form, change)
+    #
+
+admin.site.register(Book, BookAdmin)
