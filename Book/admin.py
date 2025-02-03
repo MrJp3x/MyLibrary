3cdba7c9845530ee0b9f86e7a1560a9c05cabfa9
Pedram <amirabbasasgary9731@gmail.com>
2025-02-03 14:04:16 +0330

    feat: Basic category implemented.

--- Book/admin.py
+++ Book/admin.py
@@ -1,22 +1,14 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Category
 
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'publisher')
 
 
-    # list_filter = ('publisher', 'author')
-    # search_fields = ('title', 'author')
-    # prepopulated_fields = {'slug': ('title',)}
-    # date_hierarchy = 'publisher'
-    # ordering = ('publisher', 'title')
-    # empty_value_display = '-empty-'
-    # def save_model(self, request, obj, form, change):
-    #     obj.author = request.user
-    #     obj.publisher = request.user
-    #     obj.save()
-    #     super(BookAdmin, self).save_model(request, obj, form, change)
-    #
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Category, CategoryAdmin)
--- Book/models.py
+++ Book/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     translator = models.CharField(max_length=100, blank=True, null=True)
     publisher = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None,blank=False, null=False)
     rating = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5)],
                                default=0, help_text="0 to 5")
     year = models.IntegerField()
@@ -18,4 +26,3 @@ class Book(models.Model):
 
     class Meta:
         verbose_name = 'Book'
-
