069aa514523a0a495d26d3cb9a1652855f732aaf
Pedram <amirabbasasgary9731@gmail.com>
2025-02-04 13:11:39 +0330

    chore: delete some commented code.

--- Book/serializer.py
+++ Book/serializer.py
@@ -6,7 +6,3 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
-        # depth = 1
-        # read_only_fields = ('created_at', 'updated_at')
-        # write_only_fields = ('created_at', 'updated_at')
-
